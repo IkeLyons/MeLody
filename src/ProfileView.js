9d1d4d61b68e760eb6178dd8089ef78fd97f5dd4
Ike Lyons <lyonsw@rpi.edu>
2021-10-19 16:14:08 -0400

    created 3 starting views

---
+++ src/ProfileView.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class ProfileView extends React.Component {
+  render() {
+    return <p>Profile Info!</p>;
+  }
+}
+
+export default ProfileView;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import FriendsView from "./FriendsView";
+import PlaylistView from "./PlaylistView";
+import ProfileView from "./ProfileView";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FriendsView />
+    <PlaylistView />
+    <ProfileView />
   </React.StrictMode>,
   document.getElementById("root")
 );
