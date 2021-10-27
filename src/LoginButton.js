98fcfa9eae8845bab3d46c8755501bf631e7ecdc
Ike Lyons <lyonsw@rpi.edu>
2021-10-27 18:34:23 -0400

    get the access code after sign in and 'use' it

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -1,17 +1,23 @@
 import React from "react";
 import spotifyHelper from "./spotifyHelper.js";
 
+const code = new URLSearchParams(window.location.search).get("code");
+
 class LoginButton extends React.Component {
   render() {
     return (
       <div>
-        <div>
-          <div id="login">
-            <h1>First, log in to spotify</h1>
-            <a href={spotifyHelper}>Log in</a>
+        {!code ? (
+          <div>
+            <div id="login">
+              <h1>First, log in to spotify</h1>
+              <a href={spotifyHelper}>Log in</a>
+            </div>
+            <div id="loggedin"></div>
           </div>
-          <div id="loggedin"></div>
-        </div>
+        ) : (
+          <p>{code}</p>
+        )}
       </div>
     );
   }
