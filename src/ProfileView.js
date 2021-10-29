f37fb31c8c3f78bb8513943976878100fc281dc1
brandon-red <brandonred627@gmail.com>
2021-10-28 22:52:26 -0400

    beginning dev on profile view

--- src/ProfileView.js
+++ src/ProfileView.js
@@ -1,8 +1,43 @@
 import React from "react";
+import styles from "./ProfileView.css"
+import settingsGear from "./settingsgear.png"
+import userPic from "./blankprofilepic.png"
+import {Button} from "primereact/button"
+import {Avatar} from "primereact/avatar"
 
 class ProfileView extends React.Component {
   render() {
-    return <p>Profile Info!</p>;
+    return( 
+    	<div className = {styles.profileContainer}>
+    		<p>Profile Info!</p>
+    		<div className = "upperButtons">
+    			<Button label = "Edit Profile"/>
+    			<button>
+    				<img src = {settingsGear}/>
+    			</button>
+    		</div>
+
+    		<div className = "userInfo">
+    			<h1>Username</h1>
+    			<Avatar image = {userPic} shape = "circle"/>
+    		</div>
+
+    		<div className = "genreInfo">
+    		</div>
+
+    		<div className = "Anthem">
+    		</div>
+
+    		<div className = "aboutMe">
+    		</div>
+
+    		<div className = "topTracks">
+    		</div>
+
+    		<div className = "mesh">
+    		</div>
+ 		</div> 
+    )
   }
 }
 
