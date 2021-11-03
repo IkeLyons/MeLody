3d9ea6c5806f9d5d2a24cd82aeabc83dda4cf0d1
brandon-red <brandonred627@gmail.com>
2021-11-03 17:20:18 -0400

    styling of the upper buttons on profile adjusted

--- src/ProfileView.js
+++ src/ProfileView.js
@@ -1,17 +1,14 @@
 import React from "react";
 import styles from "./ProfileView.css"
 import userPic from "./blankprofilepic.png"
-import { Button } from "primereact/button"
-import { Avatar } from "primereact/avatar"
 import { FaCog } from "react-icons/fa"
 
 class ProfileView extends React.Component {
   render() {
     return( 
     	<div className = "profileContainer">
-    		<p>Profile Info!</p>
     		<div className = "upperButtons">
-    			<Button label = "Edit Profile"/>
+    			<a href = "#"><button className = "editProfile">Edit Profile</button></a>
     			<a href = "#" id = "settings"><FaCog /></a>
     		</div>
 
